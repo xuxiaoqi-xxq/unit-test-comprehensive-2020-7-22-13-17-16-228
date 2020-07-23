c750003fd607680b1d5012ac9b83564ab08a2833
xxq <xxq_work@163.com>
2020-07-23 22:31:01 +0800

    [eva] refactor: refactor should_return_2A2B_when_answer_is_1234_given_1243 and should_return_4A0B_when_answer_is_1234_given_1234 use GenerateAnswer

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -2,6 +2,12 @@ package example;
 
 public class GuessNumberGame {
 
+    private String answer;
+
+    public GuessNumberGame(String answer) {
+        this.answer = answer;
+    }
+
     public String guess(String guessNumber) {
         if (guessNumber.equals("1243")) {
             return "2A2B";
--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -1,15 +1,20 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 public class GuessNumberGameTest {
 
   @Test
   void should_return_4A0B_when_answer_is_1234_given_1234() {
       //given
-      String answer = "1234";
-      GuessNumberGame guessNumberGame = new GuessNumberGame();
+      GenerateAnswer generateAnswer = Mockito.mock(GenerateAnswer.class);
+      when(generateAnswer.generate()).thenReturn("1234");
+      GuessNumberGame guessNumberGame = new GuessNumberGame(generateAnswer.generate());
 
       //when
       String guessResult = guessNumberGame.guess("1234");
@@ -21,8 +26,9 @@ public class GuessNumberGameTest {
     @Test
     void should_return_2A2B_when_answer_is_1234_given_1243() {
         //given
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        GenerateAnswer generateAnswer = Mockito.mock(GenerateAnswer.class);
+        given(generateAnswer.generate()).willReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(generateAnswer.generate());
 
         //when
         String guessResult = guessNumberGame.guess("1243");
