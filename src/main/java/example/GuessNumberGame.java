2419ee80c41e727106e4bcd72b52b5beaf0e25e3
xxq <xxq_work@163.com>
2020-07-23 21:33:48 +0800

    [eva] feature: implement when answer is 1234 given 1243 will return 2A2B

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -3,6 +3,9 @@ package example;
 public class GuessNumberGame {
 
     public String guess(String guessNumber) {
+        if (guessNumber.equals("1243")) {
+            return "2A2B";
+        }
         return "4A0B";
     }
 }
