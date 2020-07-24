6d921366a3868eeb47b0ed3b621ccbcd18231222
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-24 17:30:37 +0800

    [eva] refactor: refactor all

---
+++ src/main/java/example/GenerateAnswerImpl.java
@@ -0,0 +1,22 @@
+package example;
+
+import com.sun.deploy.util.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GenerateAnswerImpl implements GenerateAnswer {
+    @Override
+    public String generate() {
+        Set<Integer> answer = new HashSet<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (answer.size() < 4) {
+            Integer ans = (int) (Math.random() * 10);
+            if (!answer.contains(ans)) {
+                stringBuilder.append(String.valueOf(ans));
+            }
+            answer.add(ans);
+        }
+        return stringBuilder.toString();
+    }
+}
