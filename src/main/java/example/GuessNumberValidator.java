fab388ae7ac8305891d9a583b115859008ac636b
xxq <xxq_work@163.com>
2020-07-25 17:31:36 +0800

    [eva] feature: implement validate given a123 return false

--- src/main/java/example/GuessNumberValidator.java
+++ src/main/java/example/GuessNumberValidator.java
@@ -6,6 +6,9 @@ public class GuessNumberValidator implements Validator{
         if (guessNumber.length() != 4) {
             return false;
         }
+        if (guessNumber.matches(".*[^0-9]+.*")) {
+            return false;
+        }
         for (int i = 0; i < guessNumber.length(); i++) {
             for (int j = i + 1; j < guessNumber.length(); j++) {
                 if (guessNumber.charAt(i) == guessNumber.charAt(j)) {
