ef69de5ae6c7c6bf230b9842281890fe332b4429
xxq <xxq_work@163.com>
2020-07-26 00:23:58 +0800

    [eva] feature: add test given 11 and three valid guess numbers then output wrong input and win

--- src/test/java/example/PlayGuessNumberGameTest.java
+++ src/test/java/example/PlayGuessNumberGameTest.java
@@ -11,6 +11,7 @@ import java.io.PrintStream;
 import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class PlayGuessNumberGameTest {
@@ -43,6 +44,7 @@ public class PlayGuessNumberGameTest {
         when(generateAnswer.generate()).thenReturn("1234");
 
         guessNumberGame = Mockito.mock(GuessNumberGame.class);
+        // mock
         Field guessNumberGameField = playGuessNumberGame.getClass().getDeclaredField("guessNumberGame");
         guessNumberGameField.setAccessible(true);
         guessNumberGameField.set(playGuessNumberGame, guessNumberGame);
@@ -58,6 +60,31 @@ public class PlayGuessNumberGameTest {
             when(validator.isValid(guessNumbers[i])).thenReturn(true);
             when(guessNumberGame.guess(guessNumbers[i])).thenReturn(guessResults[i]);
 
+            guessInputFromConsole = new ByteArrayInputStream(guessNumbers[i].getBytes());
+            System.setIn(guessInputFromConsole);
+
+            //when
+            playGuessNumberGame.play();
+
+            //then
+            assertEquals(guessResults[i], guessOutputContent.toString());
+            guessOutputContent.reset();
+        }
+    }
+
+    @Test
+    void should_win_when_answer_is_1234_given_11_and_two_valid_numbers() {
+        //given
+        String[] guessNumbers = new String[]{"11", "1324", "1234"};
+        String[] guessResults = new String[]{"wrong input", "2A2B", "4A0B"};
+
+        for (int i = 0; i < guessNumbers.length; i++) {
+            if (guessNumbers[i].equals("11")) {
+                when(validator.isValid(guessNumbers[i])).thenReturn(false);
+            } else {
+                when(validator.isValid(guessNumbers[i])).thenReturn(true);
+            }
+            when(guessNumberGame.guess(guessNumbers[i])).thenReturn(guessResults[i]);
             guessInputFromConsole = new ByteArrayInputStream(guessNumbers[i].getBytes());
             System.setIn(guessInputFromConsole);
 
