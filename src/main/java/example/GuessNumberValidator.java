3c7ad509c246e0b20a6c29289d2c05534ac7f148
xxq <xxq_work@163.com>
2020-07-27 19:58:03 +0800

    [eva] refactor: add needed comments

--- src/main/java/example/GuessNumberValidator.java
+++ src/main/java/example/GuessNumberValidator.java
@@ -6,9 +6,11 @@ public class GuessNumberValidator implements Validator {
         if (guessNumber.length() != 4) {
             return false;
         }
+        // check guess number is only contains numbers
         if (guessNumber.matches(".*[^0-9]+.*")) {
             return false;
         }
+        // check guess number is repetitive
         if (guessNumber.matches(".*(.).*\\1.*")) {
           return false;
         }
