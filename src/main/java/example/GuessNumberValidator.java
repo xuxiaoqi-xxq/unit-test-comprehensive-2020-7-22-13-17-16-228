ed63716ed12ae21012e31deef7a9d9728c2033b7
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-28 19:00:19 +0800

    [eva] refactor: extract const

--- src/main/java/example/GuessNumberValidator.java
+++ src/main/java/example/GuessNumberValidator.java
@@ -1,19 +1,21 @@
 package example;
 
 public class GuessNumberValidator implements Validator {
+
+    public static final String ONLY_NUMBER_REGEX = ".*[^0-9]+.*";
+    public static final String NO_REPEAT_NUMBER_REGEX = ".*(.).*\\1.*";
+
     @Override
     public boolean isValid(String guessNumber) {
         if (guessNumber.length() != 4) {
             return false;
         }
         // check guess number is only contains numbers
-        //todo
-        if (guessNumber.matches(".*[^0-9]+.*")) {
+        if (guessNumber.matches(ONLY_NUMBER_REGEX)) {
             return false;
         }
         // check guess number is repetitive
-        //todo
-        if (guessNumber.matches(".*(.).*\\1.*")) {
+        if (guessNumber.matches(NO_REPEAT_NUMBER_REGEX)) {
           return false;
         }
         return true;
